d3f2e9377dedd0fba3d022d0b94d645792409d98
panliang003 <panliang003@ke.com>
2020-11-10 10:29:13 +0800

    add array simulate

--- code/ArraySimulate.ts
+++ code/ArraySimulate.ts
@@ -1,3 +1,5 @@
 function ArraySimulate(target: (string | number) []) {
 	
 }
+
+ArraySimulate([1, 1, 2, 2, 3, 4, 5, 6]);
--- dist/ArraySimulate.js
+++ dist/ArraySimulate.js
@@ -1,3 +1,4 @@
 "use strict";
 function ArraySimulate(target) {
 }
+ArraySimulate([1, 1, 2, 2, 3, 4, 5, 6]);
